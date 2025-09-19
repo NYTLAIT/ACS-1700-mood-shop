390d3ac2bf05380870a979d17391b3e49d51f756
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2025-09-19 15:15:00 -0700

    displayed moods wih button using javascript (stretch activity already done--next time,commit before trying stretch)

--- scripts.js
+++ scripts.js
@@ -1,14 +1,26 @@
 import data from './data.js'
 const itemsContainer = document.querySelector('#items')
 
-for (let i = 0; i < data.length; i +=1) {
+data.forEach(function (item) {
     const newDiv = document.createElement('div');
     newDiv.className = 'item'
     const img = document.createElement('img')
-    img.src = data[i].image
+    img.src = item.image
     img.width = 300
     img.height = 300
     newDiv.appendChild(img)
     console.log(img)
     itemsContainer.appendChild(newDiv)
-}
+
+    const desc = document.createElement('p')
+    desc.innerText = item.desc
+    newDiv.appendChild(desc)
+    const price = document.createElement('p')
+    price.innerText = item.price
+    newDiv.appendChild(price)
+
+    const button = document.createElement('button')
+    button.dataset.id = item.name
+    button.innerHTML = "Add to Cart"
+    newDiv.appendChild(button)
+})
