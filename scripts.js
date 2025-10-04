73252f8173c208a0a779ebae4d7c25823b79692c
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2025-10-03 20:03:59 -0700

    displayed cart items on console

--- scripts.js
+++ scripts.js
@@ -20,13 +20,16 @@ data.forEach(function (item) {
     newDiv.appendChild(price)
 
     const button = document.createElement('button')
-    button.dataset.id = item.name
-    button.innerHTML = "Add to Cart"
-    newDiv.appendChild(button)
+    button.innerHTML = "Add to Cart" 
     button.className= 'add-to-cart'
+    button.dataset.id = item.name
+    button.dataset.price = item.price
+    newDiv.appendChild(button)
+
 })
 
 const cart = []
+
 const addItemToCart = (id, price) => {
     for (let i = 0; i < cart.length; i += 1) {
         if (cart[i].id === id) {
@@ -37,26 +40,18 @@ const addItemToCart = (id, price) => {
     cart.push({ id, price, qty: 1 })
 }
 
-document.body.addEventListener('click', (e) => {
-    if (e.target.matches('.add-to-cart')) {
-        console.log(e.target)
-        addItemToCart(e.target.id, e.target.dataset.price)
-        console.log(cart)
-    }
-})
-
-/* const displayCart = () => {
+const displayCart = () => {
     let cartStr = ''
     for (let i = 0; i< cart.length; i += 1 ) {
-         const item = cart[i]
-         cartStr += `<li>
-            <span>$(item.id)</span>
-            <input type="number" value="${item.id}" class="input-qty" data-id="${item.id}"
+        const item = cart[i]
+        cartStr += `<li>
+            <span>${item.id}</span>
+            <input type="number" value="${item.qty}" class="input-qty" data-id="${item.id}">
             <span>${item.price}</span>
-            <span>${(item.price * item.qty)}.toFixed(2)}</span>
+            <span>${(item.price * item.qty).toFixed(2)}</span>
             <button class="button-add" data-id="${item.id}">+</button>
             <button class="button-sub" data-id="${item.id}">-</button>
-         </li>`
+        </li>`
     const cartItems = document.querySelector('#cart-items')
     cartItems.innerHTML = cartStr
     }
@@ -64,7 +59,8 @@ document.body.addEventListener('click', (e) => {
 
 document.body.addEventListener('click', (e) => {
     if (e.target.matches('.add-to-cart')) {
-        addItemtoCart(e.target.id, e.target.dataset.price)
-        dislplayCart()
+        addItemToCart(e.target.dataset.id, e.target.dataset.price)
+        displayCart()
+        console.log(cart)
     }
-}) */
+})
